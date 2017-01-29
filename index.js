d88c8594df98035a762e3298387220662b51cfbf
Dmitrij Fedorenko <c0va23@gmail.com>
2017-01-29 18:43:31 +0300

    Fix standart warnings

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient
 
-function MongoGridAdapter(options) {
-  options = options || {};
-  this._mongoURL = options.mongoURL;
-  if(!this._mongoURL) {
-    throw new Error("option mongoURL is required");
+function MongoGridAdapter (options) {
+  options = options || {}
+  this._mongoURL = options.mongoURL
+  if (!this._mongoURL) {
+    throw new Error('option "mongoURL" is required')
   }
 }
 
-module.exports = MongoGridAdapter;
+module.exports = MongoGridAdapter
--- test/index.js
+++ test/index.js
@@ -1,31 +1,39 @@
-const assert = require('assert');
+'use strict'
 
-const MongoGridAdapter = require('../index');
+const mocha = require('mocha')
+const describe = mocha.describe
+const it = mocha.it
 
-describe('MongoGridAdapter', function() {
-  describe('constructor', function() {
-    it('raise exception if not pass arguments', function() {
-      assert.throws(function() {
-        new MongoGridAdapter();
-      });
-    });
+const assert = require('assert')
 
-    it('raise exception if pass object without mongoURL field', function() {
-      assert.throws(function() {
-        new MongoGridAdapter({});
-      });
-    });
+const MongoGridAdapter = require('../index')
 
-    it('raise exception if pass object with empty mongoURL field', function() {
-      assert.throws(function() {
-        new MongoGridAdapter({mongoURL: null});
-      });
-    });
+describe('MongoGridAdapter', function () {
+  describe('constructor', function () {
+    const createAdapter = options => new MongoGridAdapter(options)
 
-    it('not raise exception if pass object with mongoURL field', function() {
-      assert.doesNotThrow(function() {
-        new MongoGridAdapter({mongoURL: 'mongodb url'});
-      });
-    });
-  });
-});
+    it('raise exception if not pass arguments', function () {
+      assert.throws(function () {
+        createAdapter()
+      })
+    })
+
+    it('raise exception if pass object without mongoURL field', function () {
+      assert.throws(function () {
+        createAdapter({})
+      })
+    })
+
+    it('raise exception if pass object with empty mongoURL field', function () {
+      assert.throws(function () {
+        createAdapter({mongoURL: null})
+      })
+    })
+
+    it('not raise exception if pass object with mongoURL field', function () {
+      assert.doesNotThrow(function () {
+        createAdapter({mongoURL: 'mongodb url'})
+      })
+    })
+  })
+})
